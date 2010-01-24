53f7b2919a083597ca3181d91b77ad730ae9151b
Martin Sivák <mars@montik.net>
2010-01-25 00:52:56 +0100

    inital emacs config

---
+++ ryan-misc.el
@@ -0,0 +1,26 @@
+;Reload .emacs on the fly
+(defun reload-dot-emacs()
+  (interactive)
+  (if(bufferp (get-file-buffer ".emacs"))
+      (save-buffer(get-buffer ".emacs")))
+  (load-file "~/.emacs")
+  (message ".emacs reloaded successfully"))
+
+;;Place all backup copies of files in a common location
+(defconst use-backup-dir t)   
+(setq backup-directory-alist (quote ((".*" . "~/.emacs.d/backup/")))
+      version-control t                ; Use version numbers for backups
+      kept-new-versions 16             ; Number of newest versions to keep
+      kept-old-versions 2              ; Number of oldest versions to keep
+      delete-old-versions t            ; Ask to delete excess backup versions?
+      backup-by-copying-when-linked t) ; Copy linked files, don't rename.
+
+;Never put tabs in files, use spaces instead
+;Note: Use C-q C-i to put a real tab should the need ever arise.
+(setq-default indent-tabs-mode nil)
+
+;;Allow fetching files from HTTP servers
+(url-handler-mode)
+
+;;TRAMP should default to ssh
+(setq tramp-default-method "ssh")
