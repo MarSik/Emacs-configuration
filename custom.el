fd414addb2471950abeab5d8c9f9f2fffd855573
Martin Sivak <msivak@MaMac.local>
2010-09-29 08:31:06 +0200

    Disable Option as Meta in Aquamacs

--- custom.el
+++ custom.el
@@ -9,6 +9,14 @@
  '(ecb-tip-of-the-day nil)
  '(ecb-tree-indent 2)
  '(erc-modules (quote (autojoin button completion fill irccontrols list match menu move-to-prompt netsplit networks noncommands readonly ring scrolltobottom stamp track))))
+ '(aquamacs-additional-fontsets nil t)
+ '(aquamacs-customization-version-id 210 t)
+ '(aquamacs-tool-bar-user-customization nil t)
+ '(default-frame-alist (quote ((foreground-color . "Black") (background-color . "White") (cursor-type . box) (cursor-color . "Red") (vertical-scroll-bars . right) (internal-border-width . 0) (left-fringe . 1) (right-fringe) (fringe) (menu-bar-lines . 1) (tool-bar-lines . 0))))
+ '(ns-alternate-modifier nil)
+ '(ns-tool-bar-display-mode nil t)
+ '(ns-tool-bar-size-mode nil t)
+ '(visual-line-mode nil t))
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
