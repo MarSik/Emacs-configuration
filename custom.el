9d0d2133857a66a1400e3cdbdd9723308ddf0f0b
Martin Sivak <msivak@redhat.com>
2010-12-22 16:40:01 +0100

    ECB window layout

---
+++ custom.el
@@ -0,0 +1,17 @@
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(ecb-layout-window-sizes (quote (("left8" (ecb-directories-buffer-name 0.2840909090909091 . 0.2898550724637681) (ecb-sources-buffer-name 0.2840909090909091 . 0.2318840579710145) (ecb-methods-buffer-name 0.2840909090909091 . 0.2898550724637681) (ecb-history-buffer-name 0.2840909090909091 . 0.17391304347826086)))))
+ '(ecb-options-version "2.40")
+ '(ecb-source-path nil)
+ '(ecb-tip-of-the-day nil)
+ '(ecb-tree-indent 2)
+ '(erc-modules (quote (autojoin button completion fill irccontrols list match menu move-to-prompt netsplit networks noncommands readonly ring scrolltobottom stamp track))))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
