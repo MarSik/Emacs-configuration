36c602bb4f6cd0e96c47058d98cd5281a0f6a2f5
Martin Sivak <msivak@redhat.com>
2012-08-01 13:56:04 +0200

    restructure the config file, configure yasnippet better

--- ryan-nxml.el
+++
@@ -1,5 +0,0 @@
-(add-to-list 'auto-mode-alist
-	     (cons (concat "\\." (regexp-opt '("xml" "xsd" "sch" "rng" "xslt" "svg" "rss" "xhtml") t) "\\'")
-		   'nxml-mode))
-
-(fset 'xml-mode 'nxml-mode)
--- ryan-shell.el
+++
@@ -1,8 +0,0 @@
-;; Fix colors (like ls --color, etc)
-;;; Shell mode
-(setq ansi-color-names-vector ; better contrast colors
-     ["black" "red4" "green4" "yellow4"
-       "blue3" "magenta4" "cyan4" "white"])
-(add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
-;;Make the prompt read only
-(setq comint-prompt-read-only t)
