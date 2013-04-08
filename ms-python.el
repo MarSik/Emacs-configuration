effb70528f4c76262e0a012544c863cc65160655
Martin Sivak <msivak@redhat.com>
2013-04-08 16:45:10 +0200

    Cleanup python config and use python.el mode

--- ms-python.el
+++ ms-python.el
@@ -1,61 +1,23 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Python mode customizations
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(autoload 'python-mode "python-mode" "Python Mode." t)
+(require 'python)
 (add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
 (add-to-list 'interpreter-mode-alist '("python" . python-mode))
-(setq interpreter-mode-alist
-      (cons '("python" . python-mode)
-            interpreter-mode-alist)
-      python-mode-hook
-      '(lambda () (progn
-                    (set-variable 'py-indent-offset 4)
-                    (set-variable 'py-smart-indentation nil)
-                    (set-variable 'indent-tabs-mode nil)
-                    ;;(highlight-beyond-fill-column)
-                    (define-key python-mode-map "\C-m" 'newline-and-indent)
-                    (abbrev-mode)
-                    (yas/advise-indent-function 'indent-for-tab-command)
-                    )
-      )
+
+(defun ms-python-hook ()
+  (setq python-indent-offset 4)
+  (setq python-smart-indentation nil)
+  (setq indent-tabs-mode nil)
+  ;;(highlight-beyond-fill-column)
+  (abbrev-mode)
+  ;;(yas/advise-indent-function 'indent-for-tab-command)
 )
+
+(add-hook 'python-mode-hook 'ms-python-hook)
 
 
 ;; pymacs
 (require 'pymacs)
 (pymacs-load "ropemacs" "rope-")
 (setq ropemacs-enable-autoimport t)
-
-;; (brm-init)
-(require 'pycomplete)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Put the following in your .emacs so that the
-;; abbrev table is set correctly in all modes.
-;; (Not just for java)
-;;
-;; (add-hook 'pre-abbrev-expand-hook 'abbrev-table-change)
-;; (defun abbrev-table-change (&optional args)
-;;   (setq local-abbrev-table
-;; 	(if (eq major-mode 'jde-mode)
-;; 	    (if (jde-parse-comment-or-quoted-p)
-;; 		text-mode-abbrev-table
-;; 	      java-mode-abbrev-table)
-;; 	  (if (eq major-mode 'python-mode)
-;; 	      (if (inside-comment-p)
-;; 		  text-mode-abbrev-table
-;; 		python-mode-abbrev-table
-;; 		))))
-;;   )
-;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Snippets
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;(snippet-with-abbrev-table 'python-mode-abbrev-table
-;			   ("hdr" .  "#!/usr/bin/env python\n# -*- coding: utf-8 -*-\n\"\"\"\n\"\"\"\n\n__author__ = \"Ryan McGuire <ryan@enigmacurry.com>\"")
-;			   ("main" .  "if __name__ == \"__main__\":\n$>")
-;			   ("cls" . "class $${class_name}($${object}):\n$>\"\"\"\n$>$${class documentation}\n$>\"\"\"\n$>$.")
-;			   )
