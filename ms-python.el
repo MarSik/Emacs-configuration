4c6b1c48c1fec92f866a7422118a8182cb981d2a
Martin Sivak <msivak@redhat.com>
2013-04-11 15:24:55 +0200

    Make eclim the sole source of Java information and use Jedi for Python (instead of Rope)

--- ms-python.el
+++ ms-python.el
@@ -5,6 +5,14 @@
 (add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
 (add-to-list 'interpreter-mode-alist '("python" . python-mode))
 
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/emacs-ctable/"))
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/emacs-deferred/"))
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/emacs-epc/"))
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/emacs-jedi/"))
+
+;(setq 'jedi:complete-on-dot t)
+(autoload 'jedi:setup "jedi" nil t)
+
 (defun ms-python-hook ()
   (setq python-indent-offset 4)
   (setq python-smart-indentation nil)
@@ -12,12 +20,21 @@
   ;;(highlight-beyond-fill-column)
   (abbrev-mode)
   ;;(yas/advise-indent-function 'indent-for-tab-command)
+
+  (local-set-key (kbd "\C-c?") 'jedi:show-doc)
+  (local-set-key (kbd "\C-cp") 'semantic-analyze-proto-impl-toggle)
+  (local-set-key (kbd "\C-c \C-r") 'semantic-symref)
+  (local-set-key (kbd "\C-xj") 'jedi:goto-definition)
+  ;(add-to-list 'ac-sources 'ac-source-gtags)
+  ;(add-to-list 'ac-sources 'ac-source-semantic)
+  (local-set-key [(control return)] 'jedi:complete)
+
 )
 
+(add-hook 'python-mode-hook 'jedi:setup)
 (add-hook 'python-mode-hook 'ms-python-hook)
 
-
 ;; pymacs
-(require 'pymacs)
-(pymacs-load "ropemacs" "rope-")
-(setq ropemacs-enable-autoimport t)
+;(require 'pymacs)
+;(pymacs-load "ropemacs" "rope-")
+;(setq ropemacs-enable-autoimport t)
