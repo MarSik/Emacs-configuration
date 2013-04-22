28b676c769efe865fe5c15ffb394d61a8b2f1936
Martin Sivak <msivak@redhat.com>
2013-04-22 17:13:01 +0200

    Update setup script

--- setup.sh
+++ setup.sh
@@ -7,6 +7,32 @@ git submodule init
 git submodule update --rebase
 popd
 
+# I use my own flymake version
+# sudo rm -f /usr/share/emacs/24.*/lisp/progmodes/flymake.el*
+
+# Setup Python environment
+sudo easy_install jedi epc
+
+# Setup java environment
+# install eclipse and m2e plugin
+git clone git://github.com/ervandew/eclim.git
+pushd eclim
+ant clean deploy -Declipse.home=/usr/lib64/eclipse
+popd
+
+cat >~/.eclimrc <<EOF
+# workspace dir
+osgi.instance.area.default=@user.home/Work/
+
+# increase heap size
+-Xmx256M
+
+# increase perm gen size
+-XX:PermSize=64m
+-XX:MaxPermSize=128m
+EOF
+
+
 ln -sf $PWD/dot-emacs $HOME/.emacs
 cat >>$HOME/.bashrc <<EOF
 
